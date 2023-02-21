55678ac35d759901f99eaff14122fdca97c0663d
ebi-ebi-dev <ebi.ebi.dev@gmail.com>
2023-02-21 11:24:42 +0900

    add json upload, show, and auto delete

--- dummydatagenerator/gen_product_and_random/models.py
+++ dummydatagenerator/gen_product_and_random/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from django.core.validators import FileExtensionValidator
+from django.shortcuts import render, get_object_or_404
 
 # Create your models here.
 
 class Post(models.Model):
-    document = models.FileField(upload_to='documents/')
+    title = models.CharField(default='json file', max_length=50)
+    document = models.FileField(upload_to='uploads/%Y/%m/%d/', validators=[FileExtensionValidator(['json',])])
     uploaded_at = models.DateTimeField(auto_now_add=True)
--- dummydatagenerator/gen_product_and_random/views.py
+++ dummydatagenerator/gen_product_and_random/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.shortcuts import redirect
 from .forms import DocumentForm
@@ -6,16 +6,35 @@ from .forms import DocumentForm
 # Create your views here.
 def frontpage(request):
     posts = Post.objects.all()
-    print(posts.values())
-    return render(request, "frontpage.html", {"posts": posts})
+    if request.method == 'POST':
+        form = DocumentForm(request.POST, request.FILES)
+        print(form["document"]) 
+        if form.is_valid():
+            form.save()
+            tmp2 = posts.exclude(id = posts.latest('id').id)
+            for tmp in tmp2:
+                delete(tmp.id)
+            print(posts)
+            with posts[len(posts) - 1].document.open("r") as f:
+                text = f.read()
+            return render(request, 'frontpage.html', {
+                "text": text,
+                'form': form
+            })
+    else:
+        form = DocumentForm()
+    return render(request, 'frontpage.html', {
+        "datas": posts,
+        'form': form
+    })
 
-    # if request.method == 'POST':
-    #     form = DocumentForm(request.POST, request.FILES)
-    #     if form.is_valid():
-    #         form.save()
-    #         return redirect('https://www.youtube.com/watch?v=O037g3NOoXY')
-    # else:
-    #     form = DocumentForm()
-    # return render(request, 'frontpage.html', {
-    #     'form': form
-    # })
+def delete(json_id=0):
+
+    # Uploadjsonのインスタンスを取得
+    upload_json = get_object_or_404(Post, id=json_id)
+
+    # json ファイルの実体を削除
+    upload_json.document.delete()
+    
+    # レコードの削除
+    upload_json.delete()
