551640aec11cc85e56b54da45543a50df1849770
ebi-ebi-dev <ebi.ebi.dev@gmail.com>
2023-02-27 05:16:58 +0900

    modify prod_and_random when generate_type is random

--- src/prod_and_random.py
+++ src/prod_and_random.py
@@ -12,22 +12,44 @@ class DummyDataGenerator:
             self.data = json.load(f)
         
         self.column_name_list = []
+        prod_column_list = []
+        rand_column_list = []
         for c in self.data:
             self.column_name_list.append(c["column_name"])
+            if "generate_type" in c and c["generate_type"] == "product": prod_column_list.append(c["column_name"])
+            if "generate_type" in c and c["generate_type"] == "random": rand_column_list.append(c["column_name"])
         
-        self.df_set = {}
+        self.prod_df_set = {}
+        self.rand_df_set = {}
 
         for c in self.data:
-            # 以下の場合、左辺が不成立の場合、右辺は評価されないので、キーが入っているかの評価は必ず必要。
-            # if comp1 and comp2
-            if "generate_type" in c and c["column_name"] not in self.df_set: 
-                self.df_set[c["column_name"]] = pd.DataFrame()
-            elif "linked_cname" in c and c["linked_cname"] not in self.df_set: # linkのカラムがlinkedされるカラムよりも先にjsonに記述されている場合
-                self.df_set[c["linked_cname"]] = pd.DataFrame()
-            if "generate_type" in c and c["generate_type"] == "product":
-                self.df_set[c["column_name"]][c["column_name"]] = c["generate_data"]
+            # DataFrameの箱を作る。
+            if "generate_type" in c:
+                if c["generate_type"] == "product":
+                    if c["column_name"] not in self.prod_df_set: 
+                        self.prod_df_set[c["column_name"]] = pd.DataFrame()
+                elif c["generate_type"] == "random":
+                    if c["column_name"] not in self.rand_df_set: 
+                        self.rand_df_set[c["column_name"]] = pd.DataFrame()
+            elif "linked_cname" in c: # linkのカラムがlinkedされるカラムよりも先にjsonに記述されている場合
+                if c["linked_cname"] in prod_column_list:
+                    if c["linked_cname"] not in self.prod_df_set: 
+                        self.prod_df_set[c["linked_cname"]] = pd.DataFrame()
+                elif c["linked_cname"] in rand_column_list:
+                    if c["linked_cname"] not in self.rand_df_set:
+                        self.rand_df_set[c["linked_cname"]] = pd.DataFrame()
+            # データを格納していく
+            if "generate_type" in c:
+                if c["generate_type"] == "product":
+                    self.prod_df_set[c["column_name"]][c["column_name"]] = c["generate_data"]
+                elif c["generate_type"] == "random":
+                    # print(c["column_name"], c["generate_data"])
+                    self.rand_df_set[c["column_name"]][c["column_name"]] = c["generate_data"]
             if "linked_cname" in c:
-                self.df_set[c["linked_cname"]][c["column_name"]] = c["generate_data"]
+                if c["linked_cname"] in prod_column_list:
+                    self.prod_df_set[c["linked_cname"]][c["column_name"]] = c["generate_data"]
+                elif c["linked_cname"] in rand_column_list:
+                    self.rand_df_set[c["linked_cname"]][c["column_name"]] = c["generate_data"]
 
     def json_check(data):
         # 同じカラム名はあってはだめ。
@@ -37,24 +59,26 @@ class DummyDataGenerator:
     def make_product_data(self):
 
         def recursive_merge(df_list):
-            print(len(df_list))
             if len(df_list) <=2:
-                print(df_list[0]) 
-                print(df_list[1]) 
                 return pd.merge(df_list[0], df_list[1], how = "cross")
             else:
                 return pd.merge(recursive_merge(df_list[:len(df_list)-1]), df_list[len(df_list)-1], how="cross")
 
         df_list = []
-        for v in self.df_set.values(): 
+        for v in self.prod_df_set.values(): 
             df_list.append(v)
-        print(recursive_merge(df_list))
-        
+        self.df = recursive_merge(df_list)
 
     def make_random_data(self):
-        for c in self.data:
-            if "generate_type" in c and c["generate_type"] == "random": 
-                self.df[c["column_name"]] = random.choices(c["generate_data"], k = len(self.df))
+        for v in self.rand_df_set.values():
+            tmp = random.choices(v.values.tolist(), k = len(self.df))
+            data_list = [list(x) for x in zip(*tmp)]
+            for c, i in zip(v.columns, range(len(v.columns))):
+                self.df[c] = data_list[i]
+
+        # for c in self.data:
+        #     if "generate_type" in c and c["generate_type"] == "random": 
+        #         self.df[c["column_name"]] = random.choices(c["generate_data"], k = len(self.df))
 
     def output_csv(self, output_path):
         self.df.to_csv(output_path + "/" + self.input_file_name + ".csv", index = False, encoding = "shift-jis")
