d27eafa873421936d85ffe312e4aaa98d82199cd
Tarojirotaro <une.m@wingarc.com>
2023-02-27 03:37:53 +0900

    add create part and modify prod_and_random

--- src/main.py
+++ src/main.py
@@ -3,10 +3,30 @@ import create
 
 def test_create():
     create_data = create.CreateData()
-    create_data.create_date("date", "product","2023/02/01", "2023/02/25", 1)
+    create_data.create_date("date", "product","2023/02/01", "2023/02/11", 1)
     create_data.create_link_date("enddate", "date", 1, 30)
     create_data.create_datetime("datetime", "product", "2023/02/25 00:00:00", "2023/02/25 10:00:00", 60 * 60)
     create_data.create_link_datetime("enddatetime", "datetime", 1, 59)
+    create_data.create_list("code", "product", [
+        "a",
+        "b",
+        "c"
+    ])
+    create_data.create_link_list("value", "code", [
+      "A",
+      "B",
+      "C"
+    ])
+    create_data.create_list("code2", "product", [
+        "1",
+        "2",
+        "3"
+    ])
+    create_data.create_link_list("value2", "code2", [
+        "one",
+        "two",
+        "three"
+    ])
     # create_data.create_amount("number", "random", 1, 112, 4)
     create_data.output_json("./output/input_sample_generated.json")
 
@@ -17,16 +37,15 @@ def test_prod_and_random():
     json_file = "./output/input_sample_generated.json"
     output_path = "./output"
 
-    print(json_file, output_path)
+    # print(json_file, output_path)
     
     dummydata_generator = prod_and_random.DummyDataGenerator(json_file)
     dummydata_generator.make_product_data()
     dummydata_generator.make_random_data()
     # dummydata_generator.output_csv(output_path)
-    print(dummydata_generator.df)
+    # print(dummydata_generator.df)
 
 
 if __name__ == "__main__" :
     test_create()
-    # test_prod_and_random()
-    
+    test_prod_and_random()
