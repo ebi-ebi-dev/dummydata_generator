95aebf5734702476a5cc03df0139c6fc647eb422
ebi-ebi-dev <ebi.ebi.dev@gmail.com>
2023-02-27 01:24:44 +0900

    new branch test commit

---
+++ src/create.py
@@ -0,0 +1,91 @@
+import pandas as pd
+import json
+import itertools
+import random
+import datetime
+
+class CreateData:
+
+    def __init__(self): 
+        self.data = []
+
+    def create_date(self, cname, generate_type, start, end, step = 1):
+        start = datetime.datetime.strptime(start, "%Y/%m/%d").date()
+        end = datetime.datetime.strptime(end, "%Y/%m/%d").date()
+        days_num = (end - start).days
+        datalist = [(start + datetime.timedelta(days=x)).strftime("%Y/%m/%d") for x in range(0, days_num, step)]
+        self.data.append(
+            {
+                "column_name" : cname,
+                "generate_type" : generate_type,
+                "generate_data" : datalist
+            }
+        )
+    
+    def create_link_date(self, cname, linked_cname, min, max):
+        for d in self.data:
+            if d["column_name"] == linked_cname:
+                linked_data = d["generate_data"]
+        
+        random_add = lambda tmp, min, max: datetime.datetime.strptime(tmp, "%Y/%m/%d").date() + datetime.timedelta(days=random.randint(min, max))
+        datalist = [ random_add(tmp_date, min, max).strftime("%Y/%m/%d") for tmp_date in linked_data]
+        self.data.append(
+            {
+                "column_name" : cname,
+                "linked_cname" : linked_cname,
+                "generate_data" : datalist
+            }
+        )
+    
+    def create_datetime(self, cname, generate_type, start, end, step = 1):
+        start = datetime.datetime.strptime(start, "%Y/%m/%d %H:%M:%S")
+        end = datetime.datetime.strptime(end, "%Y/%m/%d %H:%M:%S")
+        days_num = (end - start).seconds
+        datalist = [(start + datetime.timedelta(seconds=x)).strftime("%Y/%m/%d %H:%M:%S") for x in range(0, days_num, step)]
+        self.data.append(
+            {
+                "column_name" : cname,
+                "generate_type" : generate_type,
+                "generate_data" : datalist
+            }
+        )
+    
+    def create_link_datetime(self, cname, linked_cname, min, max):
+        for d in self.data:
+            if d["column_name"] == linked_cname:
+                linked_data = d["generate_data"]
+        
+        random_add = lambda tmp, min, max: datetime.datetime.strptime(tmp, "%Y/%m/%d %H:%M:%S") + datetime.timedelta(seconds=random.randint(min, max))
+        datalist = [ random_add(tmp_datetime, min, max).strftime("%Y/%m/%d %H:%M:%S") for tmp_datetime in linked_data]
+        self.data.append(
+            {
+                "column_name" : cname,
+                "linked_cname" : linked_cname,
+                "generate_data" : datalist
+            }
+        )
+    
+    def create_amount(self, cname, generate_type, start, end, step = 1):
+        datalist = [x for x in range(start, end, step)]
+        self.data.append(
+            {
+                "column_name" : cname,
+                "generate_type" : generate_type,
+                "generate_data" : datalist
+            }
+        )
+    
+    # def create_link_amount(self, cname, linked_cname, min, max):
+    #     datalist = [x for x in range(start, end, step)]
+    #     self.data.append(
+    #         {
+    #             "column_name" : cname,
+    #             "generate_type" : generate_type,
+    #             "linked_cname" : linked_cname,
+    #             "generate_data" : datalist
+    #         }
+    #     )
+    
+    def output_json(self, output_file):
+        with open(output_file, 'w') as f:
+            json.dump(self.data, f, ensure_ascii=False, indent=2)
--- src/main.py
+++ src/main.py
@@ -1,62 +1,32 @@
-import pandas as pd
-import json
-import itertools
-import random
+import prod_and_random
+import create
 
-class DummyDataGenerator:
+def test_create():
+    create_data = create.CreateData()
+    create_data.create_date("date", "product","2023/02/01", "2023/02/25", 1)
+    create_data.create_link_date("enddate", "date", 1, 30)
+    create_data.create_datetime("datetime", "product", "2023/02/25 00:00:00", "2023/02/25 10:00:00", 60 * 60)
+    create_data.create_link_datetime("enddatetime", "datetime", 1, 59)
+    # create_data.create_amount("number", "random", 1, 112, 4)
+    create_data.output_json("./output/input_sample_generated.json")
 
-    def __init__(self, json_path): 
-        self.input_file_name = json_path.split("/")[-1].split(".")[0]
-
-        with open(json_path, "r") as f:
-            self.data = json.load(f)
-        
-        self.column_name_list = []
-        for c in self.data:
-            self.column_name_list.append(c["column_name"])
-        
-        self.generate_data = []
-        self.product_column_nane_list = []
-
-        for c in self.data:
-            if c["generate_type"] == "product":
-                self.generate_data.append(c["generate_data"])
-                self.product_column_nane_list.append(c["column_name"])
-
-    def json_check(data):
-        pass
-
-    def make_product_data(self):
-        self.df = pd.DataFrame(itertools.product(*self.generate_data), columns=self.product_column_nane_list)
-
-    def make_random_data(self):
-        for c in self.data:
-            if c["generate_type"] == "random": 
-                self.df[c["column_name"]] = random.choices(c["generate_data"], k = len(self.df))
-
-    def output_csv(self, output_path):
-        self.df.to_csv(output_path + "/" + self.input_file_name + ".csv", index = False, encoding = "shift-jis")
-    
-    def get_data(self):
-        return self.df
-
-import argparse
-parser = argparse.ArgumentParser(description='このプログラムの説明（なくてもよい）')
-parser.add_argument('-json_file', "-j", help='この引数の説明（なくてもよい）')
-parser.add_argument('-output_path', "-o", help='この引数の説明（なくてもよい）')
-args = parser.parse_args()    # 4. 引数を解析
-
-def main():
-    json_file = args.json_file
-    output_path = args.output_path
+def test_prod_and_random():
+    # json_file = "./input/input_sample_noProduct.json"
+    # json_file = "./input/input_sample_noRandom.json"
+    # json_file = "./input/input_sample4.json"
+    json_file = "./output/input_sample_generated.json"
+    output_path = "./output"
 
     print(json_file, output_path)
     
-    dummydata_generator = DummyDataGenerator(json_file)
+    dummydata_generator = prod_and_random.DummyDataGenerator(json_file)
     dummydata_generator.make_product_data()
     dummydata_generator.make_random_data()
-    dummydata_generator.output_csv(output_path)
+    # dummydata_generator.output_csv(output_path)
+    print(dummydata_generator.df)
 
 
 if __name__ == "__main__" :
-    main()
+    test_create()
+    # test_prod_and_random()
+    
