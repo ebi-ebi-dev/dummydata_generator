19e8812e1b3c65cfa7c0350eab9ef19965c29b73
ebi-ebi-dev <ebi.ebi.dev@gmail.com>
2023-03-05 01:02:54 +0900

    upgrade fornt view(create): imp delete system

--- dummydatagenerator/create/static/create/create.js
+++ dummydatagenerator/create/static/create/create.js
@@ -1,6 +1,7 @@
 console.log("create.!!js")
 
 let content_area = document.getElementById('content_area');
+let id_arr = [];
 
 
 function delChild() {
@@ -13,28 +14,44 @@ function addChild() {
 
     clone.querySelectorAll("*").forEach((data, idx) =>{
         if(data.id !== "" &&  data.id !== "add_box" && data.id !== "del_box"){
-            data.id = data.id + parseInt(content_area.children.length + 1);
+            if (id_arr.length > 0){
+                data.id = data.id + parseInt(Math.max(...id_arr) + 1);
+            } else {
+                data.id = data.id + 1;
+            }
         }
         if(data.name === "column_type"){
             data.name = data.name + parseInt(content_area.children.length + 1);
+            if (id_arr.length > 1){
+                data.name = data.name + parseInt(Math.max(...id_arr) + 1);
+            } else {
+                data.name = data.name + 1;
+            }
         }
     });
 
     content_area.appendChild(clone);
     initChild();
 
+    box_forms = document.querySelectorAll(`.box`);
+    id_arr = [];
+    box_forms.forEach((d,i) =>{
+        console.log(d.id);
+        id_arr.push(parseInt(d.id.match(/[0-9]+/)[0]));
+    });
+    console.log(id_arr);
+
 }
 
 function initChild() {
 
-    
     delete_button = document.getElementsByClassName("delete_button");
-    tmp = 
-    console.log()
     delete_button[content_area.children.length-1].addEventListener('click',(e)=>{
         if (content_area.children.length > 1){
-            let idx = parseInt(e.target.id.match(/[0-9]+/)[0])-1;
-            content_area.children[idx].remove();
+            let remove_id = parseInt(e.target.id.match(/[0-9]+/)[0]);
+            let remove_idx = id_arr.indexOf(remove_id);
+            id_arr.splice(remove_idx, 1); 
+            content_area.children[remove_idx].remove();
         } else {
             window.alert("項目は必ず1つ以上必要です。");
         }
@@ -82,12 +99,6 @@ function control_visible_Ctype(obj){
             window.alert("エラー。要確認！");
         }
     }
-    // generate_type[content_area.children.length-1].addEventListener('click',(e)=>{
-    //     let idx = parseInt(e.target.id.match(/[0-9]+/)[0])-1;
-    //     if(){
-    //         content_area.children[idx].remove();
-    //     }
-    // });
 }
 
 function control_visible_Dtype(obj){
@@ -103,12 +114,6 @@ function control_visible_Dtype(obj){
 
 
 }
-
-
-// console.log(document.getElementsByClassName("column_type"));
-// document.getElementsByClassName("column_type").addEventListener("click", () => {
-//     console.log(document.getElementsByClassName("column_type"));
-// });
 
 window.addEventListener('DOMContentLoaded', function(){
     addChild();
