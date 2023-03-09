19551569d45a77316bd15d113057c49c79973432
Tarojirotaro <une.m@wingarc.com>
2023-03-09 21:16:51 +0900

    finish: product and random system

--- dummydatagenerator/gen_product_and_random/views.py
+++ dummydatagenerator/gen_product_and_random/views.py
@@ -8,66 +8,60 @@ import json
 import pandas as pd
 import io
 
+JSON_FILENAME = ""
 JSON_TEXT = ""
 OUTPUT_DF = pd.DataFrame()
 
 # Create your views here.
 def product_and_random(request):
-    global JSON_TEXT, OUTPUT_DF
-    posts = Post.objects.all()
-    # print("rrrr: ", request.FILES['document'].read().decode('utf-8'))
+    global JSON_FILENAME, JSON_TEXT, OUTPUT_DF
     if request.method == 'POST':
         if "submit" in request.POST:
             form = DocumentForm(request.POST, request.FILES)
             if form.is_valid():
-                # data = pd.read_csv(io.StringIO(request.FILES['document'].read().decode('utf-8')), delimiter=',')
-                # form.save()
-                # with posts[len(posts) - 1].document.open("r") as f:
+                JSON_FILENAME = request.FILES['document']
                 JSON_TEXT = request.FILES['document'].read().decode('utf-8')
-                # print("json: ", json.loads(text))
-                # print("data", data)
                 dummydata_generator = DummyDataGenerator()
-                dummydata_generator.read_from_jsontext(JSON_TEXT)
+                try:
+                    dummydata_generator.read_from_jsontext(JSON_TEXT)
+                except:
+                    return render(request, 'product_and_random.html', {
+                    "file": JSON_FILENAME,
+                    "text": "",
+                    'form': form,
+                    "error_msg": {"JSON_error": "JSONの形式になっていない可能性があります。ファイルを確認してください。"}
+                })
                 dummydata_generator.json_check()
                 json_dict = json.loads(JSON_TEXT)
                 return render(request, 'product_and_random.html', {
+                    "file": JSON_FILENAME,
                     "text": json_dict,
                     'form': form,
                     "error_msg": dummydata_generator.error_msg_dict
                 })
         elif "generate" in request.POST:
-            if (len(JSON_TEXT) == 0):
-                return render(request, 'product_and_random.html', {
-                    "text": "",
-                    'form': DocumentForm()
-                })
+            # if (len(JSON_TEXT) == 0):
+            #     return render(request, 'product_and_random.html', {
+            #         "text": "",
+            #         'form': DocumentForm()
+            #     })
             print("generate!")
             dummydata_generator = DummyDataGenerator()
             dummydata_generator.read_from_jsontext(JSON_TEXT)
             dummydata_generator.json_check()
             json_dict = json.loads(JSON_TEXT)
-            if(dummydata_generator.error_code != 0 ): 
-                print(dummydata_generator.error_msg)
-                json_dict = json.loads(JSON_TEXT)
             dummydata_generator.prepare_prod_and_random()
             dummydata_generator.make_product_data()
             dummydata_generator.make_random_data()
             OUTPUT_DF = dummydata_generator.df
-            print(json_dict)
             return render(request, 'product_and_random.html', {
-                    "text" : json_dict,
-                    "dataframe": OUTPUT_DF.head(10).to_html(classes=["table", "table-bordered", "table-hover, overflow-scroll"]),
-                    'form': DocumentForm()
+                "file":  JSON_FILENAME,
+                "text" : "wwww",
+                "dataframe": OUTPUT_DF.head(10).to_html(classes=["table", "table-bordered", "table-hover, overflow-scroll"]),
+                'form': DocumentForm()
             })
 
         elif "download_csv" in request.POST:
-            # with posts[len(posts) - 1].document.open("r") as f:
-            #     data = json.load(f)
-            # dummydata_generator = DummyDataGenerator(str(posts[len(posts) - 1].document.file))
-            # dummydata_generator.json_check()
-            # dummydata_generator.prepare_prod_and_random()
-            # dummydata_generator.make_product_data()
-            # dummydata_generator.make_random_data()
             response = HttpResponse(content_type='text/csv')
             response['Content-Disposition'] = 'attachment; filename=filename.csv'
             OUTPUT_DF.to_csv(path_or_buf=response, sep=',', float_format='%.2f', index=False, decimal=",")
@@ -75,17 +69,5 @@ def product_and_random(request):
     else:
         form = DocumentForm()
     return render(request, 'product_and_random.html', {
-        "datas": posts,
         'form': form
     })
-
-def delete(json_id=0):
-
-    # Uploadjsonのインスタンスを取得
-    upload_json = get_object_or_404(Post, id=json_id)
-
-    # json ファイルの実体を削除
-    upload_json.document.delete()
-    
-    # レコードの削除
-    upload_json.delete()
