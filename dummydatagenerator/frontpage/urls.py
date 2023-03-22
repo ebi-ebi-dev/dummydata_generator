cffcc19001a8bc7088ef80d387b1857deddc043d
ebi-ebi-dev <ebi.ebi.dev@gmail.com>
2023-03-22 22:55:15 +0900

    change gitignore

--- dummydatagenerator/frontpage/urls.py
+++ dummydatagenerator/frontpage/urls.py
@@ -5,5 +5,5 @@ from .views import frontpage, how_to_use
 
 urlpatterns = [
     path("", frontpage.as_view()),
-    path("manual/", how_to_use.as_view())
+    path("how_to_use/", how_to_use.as_view())
 ]
