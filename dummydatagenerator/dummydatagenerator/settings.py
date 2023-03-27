8ff0bc72d640217104a5d443101de4c43e78fc3b
Tarojirotaro <une.m@wingarc.com>
2023-03-27 12:55:17 +0900

    change setting.py: set IP at ALLOWED_HOSTS

--- dummydatagenerator/dummydatagenerator/settings.py
+++ dummydatagenerator/dummydatagenerator/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-f(m58l(3^2ag%5*y#uxthi29_x=38o90vjz%&y1cjsh$q7ydd5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["10.19.70.70"]
 
 
 # Application definition
